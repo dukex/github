5fa7e46fdb65156fe7f848e07ec84bc85a6b50eb
Duke <dukekhaos@gmail.com>
2016-10-12 19:08:54 -0300

    remove content_exists?

--- src/github/client/contents.cr
+++ src/github/client/contents.cr
@@ -1,3 +1,5 @@
+require "../response/contents"
+
 module Github
   class Client
     module Contents
@@ -7,11 +9,6 @@ module Github
 
       def content(repo, path)
         Response::Contents.from_response get(repo.contents_url.gsub("{+path}", path))
-      end
-
-      def content_exists?(repo, path)
-        response = content(repo, path)
-        response.status_code == 200
       end
     end
   end
