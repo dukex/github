3fd61ab316ed44aaf7a8abd5ef79a29265c81bcc
Duke <dukekhaos@gmail.com>
2016-10-13 02:05:12 -0300

    add support to access_token auth

--- src/github/client.cr
+++ src/github/client.cr
@@ -11,16 +11,28 @@ module Github
     include Contents
     include Users
 
+    @access_token : String?
+
+    getter :access_token
+
+    def initialize(@access_token = nil)
+    end
+
     def get(path)
       get(path, Hash(String, String).new)
     end
 
     def get(path, params : Hash(String, String))
-      http.get(path + "?" + encode_params(params))
+      path += "?" + encode_params(params) if params.size > 0
+      http.get(path)
     end
 
     private def http
-      HTTP::Client.new(ENDPOINT, 443, true)
+      http = HTTP::Client.new(ENDPOINT, 443, true)
+      http.before_request do |request|
+        request.headers["Authorization"] = "token #{access_token}" if access_token
+      end
+      http
     end
 
     private def encode_params(params)
